41d338daf40ebdb0c06eb54ccfe801ae28e7f418
vfa-khanhld <khanhld@vitalify.asia>
2022-04-29 16:36:25 +0700

    reactive, ref, watch, watchEffect, Computed

--- routervuejs/src/pages/homePage.vue
+++ routervuejs/src/pages/homePage.vue
@@ -1,3 +1,79 @@
 <template>
   <h1>HomePage</h1>
+  <!-- <p>
+    {{ `${secondName.lastName} ${secondName.middleName} ${firstName}` }}
+  </p>
+  <p>{{ car }}</p> -->
+  <input type="text" v-model="searchText" />
+  <ul>
+    <li v-for="(customer, index) in customersFilled" :key="index">
+      {{ customer }}
+    </li>
+  </ul>
+  <!-- <button @click="onChangeSomething">Change</button> -->
 </template>
+å
+<script>
+//ref co co san trong thu vien cua VueJS, va no giup
+// thg template nhan thay su thay doi cua script
+import { ref, reactive, computed, watch } from "vue";
+
+export default {
+  setup() {
+    //so sanh su khac nhau giua ref va reactive,
+    //reactive truy cap truc tiep, ref truy cap thonga qua value
+    // const firstName = ref("Khanh");
+    // //Du co khai bao ref la mot cai mang thi van phai truy cap value
+    // const secondName = ref({
+    //   middleName: "Duy",
+    //   lastName: "Le",
+    // });
+    // const car = reactive({
+    //   price: 1000,
+    //   name: "Honda",
+    // });
+
+    const searchText = ref("");
+
+    const customers = reactive([
+      "Something",
+      "Ben",
+      "Khanh",
+      "Ben1",
+      "Khanh1",
+      "Something1",
+    ]);
+    //Viet computed trong composition API
+    const customersFilled = computed(() =>
+      customers
+        .map((customer) => {
+          customer = customer.toLowerCase();
+          return customer;
+        })
+        .filter((customer) => customer.includes(searchText.value.toLowerCase()))
+    );
+
+    watch(searchText, (newValue, oldValue) => {
+      console.log(newValue, oldValue);
+    });
+
+    // function onChangeSomething() {
+    //   // console.log("running here....");
+    //   // console.log(secondName);
+    //   // console.log(car);
+    //   // car.price += 1000;
+    //   // firstName.value = "Ben";
+    // }
+
+    return {
+      searchText,
+      // secondName,
+      // firstName,
+      // onChangeSomething,
+      // car,
+      customersFilled,
+    };
+    //Khi 1 thg ref dc return thi no dc unwrap va dc tra ra value
+  },
+};
+</script>
